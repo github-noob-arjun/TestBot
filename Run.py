7777a717df15fc576de8702df532b674be906063
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-12-15 09:00:35 +0530

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,25 @@
+TOK="5931770179:AAHoonbw-4iJF6_rCzj02sph_6wNTkUssFs"
+ID="15681435"
+HAS="29021e7d8f6fe5338a45470115567f9e"
+
+import os
+from pyrogram import Client, filters
+
+Bot = Client(
+    "RunBot",
+    bot_token=TOK,
+    api_id=ID,
+    api_hash=HAS
+)
+
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    
+    await update.reply_text(
+        text="Hi Java jaba jabaa.. 💥🔥🎉🎁",
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+bot = Bot()
+bot.run()
